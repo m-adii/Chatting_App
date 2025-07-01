abcf1b7fb7c61e165010e894e8b01f30bb0b14f9
muhammad adeel <adeelahmadpsr098@gmail.com>
2025-07-01 03:41:07 -0700

    cached network image package used

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class Apis {
   //for accessing firbase authentication
   static FirebaseAuth auth = FirebaseAuth.instance;
+  
   //for accesssing cloud firestore database
   static FirebaseFirestore firestore = FirebaseFirestore.instance;
 
--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chattingapp/main.dart';
 import 'package:chattingapp/models/chat_user.dart';
 import 'package:flutter/material.dart';
@@ -20,10 +21,28 @@ class _ChatUsercardsState extends State<ChatUsercards> {
       child:InkWell(
         onTap:(){} ,
        child: ListTile(
-      leading:CircleAvatar(backgroundColor: Colors.blue,child: Icon(Icons.person_2_outlined,color: Colors.white,),),
+      // leading:CircleAvatar(backgroundColor: Colors.blue,child: Icon(Icons.person_2_outlined,color: Colors.white,),),
+      leading:ClipRRect(
+        borderRadius: BorderRadiusGeometry.circular(mq.height * .3),
+        child: CachedNetworkImage(
+          width: mq.height * .055,
+          height: mq.height * .055,
+          imageUrl:widget.user.image,
+          // placeholder: (context, url) => CircularProgressIndicator(),
+          errorWidget: (context, url, error) =>CircleAvatar(backgroundColor: Colors.blue,child: Icon(Icons.person_2_outlined,color: Colors.white,),),
+             ),
+      ),
        title: Text(widget.user.name,style: TextStyle(fontSize: 17,fontWeight: FontWeight.w400),),
        subtitle: Text(widget.user.about,maxLines: 1,),
-       trailing: Text("7:10 pm",style: TextStyle(color: Colors.black54),),
+      //  trailing: Text("7:10 pm",style: TextStyle(color: Colors.black54),),
+       trailing: Container(
+        width: 12,
+        height: 12,
+        decoration: BoxDecoration(
+          color: Colors.greenAccent.shade400, 
+          borderRadius: BorderRadius.circular(7)
+        ),
+       )
        ), 
       ),
       );
